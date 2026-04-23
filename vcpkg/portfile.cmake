72e211feca92f066f2ae2d96106e08b64dd2b6b7
Aviad Cohen Gvoora <avi27200p@gmail.com>
2026-04-23 19:54:38 +0300

    [ADD] Add hash value for package

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -5,9 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AviadCohen24/LockFreeRingBuffer
     REF "v${VERSION}"
-    SHA512 0  # TODO: replace with real SHA512 after creating the v1.0.0 GitHub release
-              # Run: vcpkg install lock-free-ring-buffer --overlay-ports=./vcpkg
-              # and copy the hash from the error message.
+    SHA512 297bd4b08fc6e6d0a16915e7ee7f6e818fc1c9b0b00b9403bc6a5f13da5644755e498649ad3698ef440a5d44264143c12cdbc15daf23b5dfc482686cdee8690e
     HEAD_REF main
 )
 
